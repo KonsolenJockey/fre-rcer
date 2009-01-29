cdf414a57c9b74c954d2ea72c25e7c813e8abf81
Volker Wegert <mail@volker-wegert.de>
2009-01-29 20:38:29 +0000

    Added creation of base installation to build system.

---
+++ net.sf.rcer.build/scripts/01-RecreateBase.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Script to (re)create the contents of the base installation.
+#
+
+export ECLIPSE="eclipse-SDK-3.4.1-linux-gtk.tar.gz"
+export OAW="org.openarchitectureware.sdk.feature-4.3.1.20090107-2000PRD.zip"
+
+if [ "x${RCER_BUILD_HOME}" = "x" ] ; then
+  echo Environment variable RCER_BUILD_HOME is not set.
+  exit 1
+fi 
+
+if [ ! -f ${RCER_BUILD_HOME}/distfiles/${ECLIPSE} ] ; then 
+	echo ${RCER_BUILD_HOME}/distfiles/${ECLIPSE} is missing. Download the file and re-run this script.
+	exit -1
+fi
+
+if [ ! -f ${RCER_BUILD_HOME}/distfiles/${OAW} ] ; then
+	echo ${RCER_BUILD_HOME}/distfiles/${OAW} is missing. Download the file and re-run this script.
+	exit -1
+fi
+
+echo "Deleting old installation..."
+mkdir -p ${RCER_BUILD_HOME}/base
+rm -rf ${RCER_BUILD_HOME}/base/*
+cd ${RCER_BUILD_HOME}/base
+
+echo "Extracting Eclipse SDK..."
+tar xzf ${RCER_BUILD_HOME}/distfiles/${ECLIPSE}
+
+echo "Extracting openArchitectureWare SDK..."
+unzip -q ${RCER_BUILD_HOME}/distfiles/${OAW}
+
+echo "OK."
