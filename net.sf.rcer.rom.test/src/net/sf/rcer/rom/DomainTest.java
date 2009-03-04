3dbb2c083c1270bdcc2872080ded34d44e369544
Volker Wegert <mail@volker-wegert.de>
2009-03-04 18:22:45 +0000

    Added some more tests for domain loading.

--- net.sf.rcer.rom.test/src/net/sf/rcer/rom/DomainTest.java
+++ net.sf.rcer.rom.test/src/net/sf/rcer/rom/DomainTest.java
@@ -8,13 +8,12 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import net.sf.rcer.conn.connections.ConnectionManager;
-import net.sf.rcer.conn.locales.LocaleRegistry;
 import net.sf.rcer.rom.ddic.DDICFactory;
+import net.sf.rcer.rom.ddic.DictionaryDataType;
 import net.sf.rcer.rom.ddic.Domain;
 import net.sf.rcer.test.provider.ConnectionProvider;
 
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.emf.common.util.EList;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -52,13 +51,72 @@ public class DomainTest {
 	 * @throws Exception
 	 */
 	@Test
-	public void testLoadDomain() throws Exception {
+	public void testDomainCHAR10() throws Exception {
+		Domain dom = DDICFactory.eINSTANCE.createDomain();
+		dom.setName("CHAR10");
+		collection.getObjects().add(dom);
+		IStatus status = dom.load();
+		assertTrue(status.getMessage(), status.isOK());
+		assertEquals("domain name", "CHAR10", dom.getName());
+		assertEquals("data type", DictionaryDataType.CHARACTER, dom.getDictionaryDataType());
+		assertEquals("length", 10, dom.getLength());
+		assertEquals("output length", 10, dom.getOutputLength());
+		assertFalse("case sensitive", dom.isCaseSensitive());
+		assertEquals("domain should not have values", 0, dom.getValues().size());
+	}
+	
+	/**
+	 * @throws Exception
+	 */
+	@Test
+	public void testDomainTEXT10() throws Exception {
+		Domain dom = DDICFactory.eINSTANCE.createDomain();
+		dom.setName("TEXT10");
+		collection.getObjects().add(dom);
+		IStatus status = dom.load();
+		assertTrue(status.getMessage(), status.isOK());
+		assertEquals("domain name", "TEXT10", dom.getName());
+		assertEquals("data type", DictionaryDataType.CHARACTER, dom.getDictionaryDataType());
+		assertEquals("length", 10, dom.getLength());
+		assertEquals("output length", 10, dom.getOutputLength());
+		assertTrue("case sensitive", dom.isCaseSensitive());
+		assertEquals("domain should not have values", 0, dom.getValues().size());
+	}
+	
+	/**
+	 * @throws Exception
+	 */
+	@Test
+	public void testDomainNUMC5() throws Exception {
+		Domain dom = DDICFactory.eINSTANCE.createDomain();
+		dom.setName("NUMC5");
+		collection.getObjects().add(dom);
+		IStatus status = dom.load();
+		assertTrue(status.getMessage(), status.isOK());
+		assertEquals("domain name", "NUMC5", dom.getName());
+		assertEquals("data type", DictionaryDataType.NUMERICAL_CHARACTER, dom.getDictionaryDataType());
+		assertEquals("length", 5, dom.getLength());
+		assertEquals("output length", 5, dom.getOutputLength());
+		assertFalse("case sensitive", dom.isCaseSensitive());
+		assertEquals("domain should not have values", 0, dom.getValues().size());
+	}
+	
+	/**
+	 * @throws Exception
+	 */
+	@Test
+	public void testDomainCHAR1_X() throws Exception {
 		Domain dom = DDICFactory.eINSTANCE.createDomain();
 		dom.setName("CHAR1_X");
 		collection.getObjects().add(dom);
 		IStatus status = dom.load();
-		
 		assertTrue(status.getMessage(), status.isOK());
+		assertEquals("domain name", "CHAR1_X", dom.getName());
+		assertEquals("data type", DictionaryDataType.CHARACTER, dom.getDictionaryDataType());
+		assertEquals("length", 1, dom.getLength());
+		assertEquals("output length", 1, dom.getOutputLength());
+		assertFalse("case sensitive", dom.isCaseSensitive());
+		assertEquals("domain should have values", 2, dom.getValues().size());
 	}
 	
 }
