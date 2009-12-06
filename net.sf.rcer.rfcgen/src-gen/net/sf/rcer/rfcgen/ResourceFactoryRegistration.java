ed11e1357082800b17db988daaff84f2893d8cb6
Volker Wegert <mail@volker-wegert.de>
2009-12-06 21:42:32 +0000

    Removed old versions of RFC generator.

--- net.sf.rcer.rfcgen/src-gen/net/sf/rcer/rfcgen/ResourceFactoryRegistration.java
+++
@@ -1,9 +0,0 @@
-package net.sf.rcer.rfcgen;
-
-import net.sf.rcer.rfcgen.resource.rfcgenResourceFactory;
-
-public class ResourceFactoryRegistration {
-	public static void register() {
-	  	rfcgenResourceFactory.register();
-	}
-}
