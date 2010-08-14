02ca7e2594204e537c4d99d51cabb840eee1e576
Volker Wegert <mail@volker-wegert.de>
2010-08-14 13:50:57 +0000

    Added zipped distribution file to web site and build process.

--- net.sf.rcer.build/scripts/04-Update-Website.sh
+++ net.sf.rcer.build/scripts/04-Update-Website.sh
@@ -20,6 +20,14 @@ if [ "x${RCER_SF_USER}" = "x" ] ; then
   exit 1
 fi 
 
+echo "Creating zipped distribution package..."
+mkdir -p ${RCER_BUILD_HOME}/website/dist
+rm -rf ${RCER_BUILD_HOME}/website/dist/*
+cp -r ${RCER_BUILD_HOME}/build/site/features ${RCER_BUILD_HOME}/website/dist/
+cp -r ${RCER_BUILD_HOME}/build/site/plugins ${RCER_BUILD_HOME}/website/dist/
+cd ${RCER_BUILD_HOME}/website/dist/
+zip -r rcer-current.zip features plugins
+
 echo "Exporting static content from repository..."
 mkdir -p ${RCER_BUILD_HOME}/website/src
 rm -rf ${RCER_BUILD_HOME}/website/src/*
@@ -31,6 +39,8 @@ mkdir ${RCER_BUILD_HOME}/website/staging
 mkdir ${RCER_BUILD_HOME}/website/staging/update-site
 cp -r ${RCER_BUILD_HOME}/website/src/net.sf.rcer.website/* ${RCER_BUILD_HOME}/website/staging/
 cp -r ${RCER_BUILD_HOME}/build/site/* ${RCER_BUILD_HOME}/website/staging/update-site
+cp ${RCER_BUILD_HOME}/website/dist/rcer-current.zip ${RCER_BUILD_HOME}/website/staging/
+
 
 echo "Updating the website..."
 rsync -avP -e ssh ${RCER_BUILD_HOME}/website/staging/* ${RCER_SF_USER},rcer@web.sourceforge.net:htdocs/
