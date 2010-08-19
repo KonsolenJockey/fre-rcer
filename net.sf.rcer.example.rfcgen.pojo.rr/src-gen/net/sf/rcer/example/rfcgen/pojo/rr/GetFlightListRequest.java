90745adbb7043c18f8ce57cea51dabc375f4a676
Volker Wegert <mail@volker-wegert.de>
2010-08-19 19:37:03 +0000

    Fixed Copyright statements.

--- net.sf.rcer.example.rfcgen.pojo.rr/src-gen/net/sf/rcer/example/rfcgen/pojo/rr/GetFlightListRequest.java
+++ net.sf.rcer.example.rfcgen.pojo.rr/src-gen/net/sf/rcer/example/rfcgen/pojo/rr/GetFlightListRequest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 The RCER Development Team.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Volker Wegert <rcer@volker-wegert.de> - initial API and implementation
+ *******************************************************************************/
  
 package net.sf.rcer.example.rfcgen.pojo.rr;
 
--- net.sf.rcer.jcoimport/src/net/sf/rcer/jcoimport/JCoImportWizard.java
+++ net.sf.rcer.jcoimport/src/net/sf/rcer/jcoimport/JCoImportWizard.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2008 The RCER Development Team.
+ * Copyright (c) 2008, 2010 The RCER Development Team.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
